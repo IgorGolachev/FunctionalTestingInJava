15f8aa7b925da4656f247e8b7e862aa1650fab0e
IgorGolachev <igorgolachev@yahoo.com>
2021-01-07 12:22:10 +0100

    :wrench: - headless capabilities

--- src/test/java/steps/Hooks.java
+++ src/test/java/steps/Hooks.java
@@ -1,6 +1,9 @@
 package steps;
 
+import com.codeborne.selenide.Configuration;
 import io.cucumber.java.Before;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import static com.codeborne.selenide.Selenide.open;
 
@@ -8,6 +11,20 @@ public final class Hooks {
 
     @Before
     public void setUp() {
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--disable-extensions");
+        chromeOptions.addArguments("--headless");
+        chromeOptions.addArguments("--no-sandbox");
+        chromeOptions.addArguments("--disable-gpu");
+        chromeOptions.addArguments("incognito");
+        capabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
+        capabilities.setAcceptInsecureCerts(true);
+        Configuration.browserCapabilities = capabilities;
+        Configuration.headless = true;
+        Configuration.startMaximized = true;
+
         open("http://automationpractice.com/index.php");
     }
 }
