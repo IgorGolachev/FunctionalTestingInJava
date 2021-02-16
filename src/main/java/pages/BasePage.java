0be6c4f5ad7542fa70a7ca84a2e95be4d06527eb
IgorGolachev <igorgolachev@yahoo.com>
2021-02-17 00:37:13 +0100

    :bug: - Dresses link locator fixed

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -3,10 +3,11 @@ package pages;
 import com.codeborne.selenide.SelenideElement;
 
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$x;
 
 public final class BasePage {
 
     public final SelenideElement signInButton = $("a[class='login']");
     public final SelenideElement currentUserLink = $("div[class='header_user_info'] a span");
-    public final SelenideElement dressesLink = $("a[title='Dresses']");
+    public final SelenideElement dressesLink = $x("(//a[@title='Dresses'])[2]");
 }
